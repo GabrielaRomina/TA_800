4add9a70beaa9f1786a390a4f2c62df414a41ea6
GabrielaRomina <gabrielarominalupas@gmail.com>
2023-07-14 18:39:13 +0200

    app

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ from langchain.llms import OpenAI
 from serpapi import GoogleSearch
 import os
 import openai
+import pymysql
 from dotenv import load_dotenv
+import datetime
 
+# Obtener la fecha actual
+fecha_actual = datetime.date.today()
 
 # Cargar las variables de entorno desde el archivo .env
 load_dotenv()
@@ -26,20 +30,53 @@ tools = load_tools(["serpapi", "llm-math"], llm=llm)
 # Initialize the agent
 agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
 
+username = "admin"
+password = "12345678"
+host = 'datagpt.cgtlbgxgrxsx.us-east-2.rds.amazonaws.com'
+port = 3306
+
+db = pymysql.connect(
+    host=host,
+    user=username,
+    password=password,
+    database="datagpt",
+    cursorclass=pymysql.cursors.DictCursor
+)
+
+cursor = db.cursor()
+
+cursor.execute('SELECT VERSION()')
+version = cursor.fetchone()
+print(f'MySQL version: {version}')
+
+create_table = '''
+CREATE TABLE IF NOT EXISTS datagpt (
+    id INT NOT NULL AUTO_INCREMENT,
+    date DATE,
+    question TEXT,
+    answer TEXT,
+    links TEXT,
+    PRIMARY KEY (id)
+)
+'''
+cursor.execute(create_table)
+
+use_db = ''' USE datagpt'''
+cursor.execute(use_db)
+
+llm = OpenAI()
+
+tools = load_tools(["serpapi", "llm-math"], llm=llm)
+
+agent = initialize_agent(
+    tools, llm, agent="zero-shot-react-description", verbose=True)
+
+
 @app.route('/api/chat', methods=['GET'])
 def chat():
-    """
-    Endpoint que responde a las solicitudes GET en '/api/chat'.
-    Recibe la pregunta como parámetro en la URL ('question').
-    Utiliza el modelo ChatGPT de OpenAI para generar una respuesta a la pregunta.
-    También realiza una búsqueda en Google utilizando la API de SerpApi y devuelve enlaces relevantes.
-
-    Returns:
-        JSON: Una respuesta JSON que contiene la respuesta generada y los enlaces relevantes.
-    """
     question = request.args.get('question')
+    fecha_actual = datetime.datetime.today()
 
-    # Buscar en Google utilizando SerpApi
     search_params = {
         "engine": "google",
         "q": question,
@@ -48,10 +85,8 @@ def chat():
     search = GoogleSearch(search_params)
     results = search.get_dict()
 
-    # Obtener los enlaces relevantes de los resultados de búsqueda
     relevant_links = [result['link'] for result in results['organic_results']]
 
-    # Generar respuesta utilizando ChatGPT
     response = openai.Completion.create(
         engine="text-davinci-003",
         prompt=question,
@@ -61,17 +96,22 @@ def chat():
         temperature=0.7
     )
 
-    # Obtener la respuesta generada
     answer = response.choices[0].text.strip()
+    links_str = ",".join(relevant_links)
+
+    insert_data = '''
+    INSERT INTO datagpt (date, question, answer, links) VALUES (%s, %s, %s, %s)
+    '''
+    cursor.execute(insert_data, (fecha_actual, question, answer, links_str))
+    db.commit()
 
     return jsonify({'answer': answer, 'relevant_links': relevant_links})
 
+
 @app.route('/')
 def index():
-    """
-    Página de inicio que muestra un template llamado 'index.html'.
-    """
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.environ.get("PORT", 5000))
